550cc54496a41e5a1aa86a0c9fac45d720947642
tomohondeveloper <rumondorjosh@gmail.com>
2025-05-08 01:31:29 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// toogle clas actve
+
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+//klik di luar side bar 
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click' , function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+});
