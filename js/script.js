dcde0a48a87c22c2ef27ceffb057d7fd69a209aa
tomohondeveloper <rumondorjosh@gmail.com>
2025-05-09 02:30:45 +0800

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-// toogle clas actve
-
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+  navbarNav.classList.toggle('active');
 };
-//klik di luar side bar 
-const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click' , function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
+
 // Modal Box
 const itemDetailModal = document.querySelector('#item-detail-modal');
 const itemDetailButtons = document.querySelectorAll('.item-detail-button');
@@ -35,3 +63,4 @@ window.onclick = (e) => {
   if (e.target === itemDetailModal) {
     itemDetailModal.style.display = 'none';
   }
+};
